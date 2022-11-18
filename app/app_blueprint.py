51c0d528ea7cb878b0d429d1870634bf2730c5eb
James Caroe <18caroej@utcsheffield.org.uk>
2022-11-18 12:00:06 +0000

    Update app_blueprint.py

--- app/app_blueprint.py
+++ app/app_blueprint.py
@@ -6,6 +6,13 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from werkzeug.exceptions import aborts
+from app.models.user import User, Role
+from app.models.exercise import Exercise
+from app.models.machine import Machine
+from app.models.measurement import Measurement
+from app.models.muscle_group import MuscleGroup
+from app.models.session import Session
+from app.models.session_exercise import SessionExercise
 
 #app_blueprint = Blueprint('app_blueprint', __name__, static_url_path="app", static_folder="/home/meggleton/Projects/demo-progressive-web-app" )
 app_blueprint = Blueprint('app_blueprint', __name__ )
@@ -17,12 +24,9 @@ def index():
 
 @app_blueprint.route('/startvalues')
 def startvalues():
-     user = User(
-            username=request.form["username"],
-            email=request.form["email"],
-        )
-        db.session.add(user)
-        db.session.commit()
+    db.session.add(MuscleGroup(name="Biceps"))
+    db.session.add(MuscleGroup(name="Triceps"))
+    db.session.commit()
 
     return render_template("app/index.html")
 
